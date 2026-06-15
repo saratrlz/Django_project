1f59e7a6f81f22223dbd3d1f483c50006d14732b
Sara Trouillez <157759091+saratrlz@users.noreply.github.com>
2026-06-15 20:00:47 +0200

    Update ALLOWED_HOSTS to allow all host

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fa^vru#+8ikz=-oc8io&is^!qnb3tg_8qkrydi+&f(-m7ndh$^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
